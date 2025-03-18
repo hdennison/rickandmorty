b6b037d4c680b2d6c60168990397a6f9b8946ca1
Harold Dennison <harold.dennison@gmail.com>
2025-03-18 21:44:03 +0100

    chore: Add React Query

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,16 @@
-export default function Home() {
-  return <main>Hello world</main>;
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
+import Home from "@/components/home/home";
+
+export default async function Page() {
+  const queryClient = new QueryClient();
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Home />
+    </HydrationBoundary>
+  );
 }
