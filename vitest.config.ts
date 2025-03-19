469d2ecd33c383652a0d6f64c4ad6ad44f3cdb2c
Harold Dennison <harold.dennison@gmail.com>
2025-03-19 14:08:31 +0100

    chore: set up UI components

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,7 +5,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   plugins: [tsconfigPaths(), react()],
   test: {
-    include: ["tests/vitest/**/*.test.tsx"],
+    include: ["src/**/*.test.tsx"],
     browser: {
       provider: "playwright",
       enabled: true,
